6db51875daca65f7a9f55761af5c91a1cc86aeb0
Magnus Settemsli Mogstad <magnusmogstad@Magnuss-MacBook-Pro.local>
2017-02-04 16:00:23 +0100

    Transfered code from react to inferno

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import Inferno from 'inferno';
 import App from './App';
 
 describe('The App component', () => {
   it('Should not crash when it loads', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    Inferno.render(<App />, div);
   });
 });
