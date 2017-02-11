e22e7831b5092099ffe5cefcea3d0fc732f1c06b
Hans-Olav Hessen <hansolav_91@hotmail.com>
2017-02-11 16:58:52 +0100

    Fixed test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,18 @@
 import Inferno from 'inferno';
+import { Provider } from 'inferno-redux';
 import App from './App';
+import createStore from './store/store';
+
 
 describe('The App component', () => {
   it('Should not crash when it loads', () => {
     const div = document.createElement('div');
-    Inferno.render(<App />, div);
+    const store = createStore();
+    Inferno.render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+
+      , div);
   });
 });
