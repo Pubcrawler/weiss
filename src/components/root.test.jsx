887034d8704a4590e3fcd922dfef70d8771caabc
Hans-Olav Hessen <hansolav_91@hotmail.com>
2017-02-12 09:52:27 +0100

    Changed from app-test to root-test

--- src/components/root.test.jsx
+++ src/components/root.test.jsx
@@ -5,7 +5,7 @@ import configureStore from '../configureStore';
 import Root from './root';
 
 
-describe('The App component', () => {
+describe('The Root component', () => {
   it('Should not crash when it loads', () => {
     const div = document.createElement('div');
     const store = configureStore();
