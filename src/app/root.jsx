c2a633f137281363ff788e8ac51c5ed5d9abca34
Håkon Åmdal <hakon.amdal@schibsted.com>
2017-04-19 21:39:11 +0200

    Create history inside the root component
    For some reason, this is needed to make the Root Component test pass.

--- src/app/root.jsx
+++ src/app/root.jsx
@@ -1,6 +1,7 @@
 import Inferno from 'inferno';
 import { Provider } from 'inferno-redux';
 import { Router, Route, IndexRoute } from 'inferno-router';
+import createBrowserHistory from 'history/createBrowserHistory';
 import App from './app.jsx';
 import StopList from '../components/stop-list/stop-list.jsx';
 import CrawlList from '../components/crawl/crawl-list.jsx';
@@ -8,9 +9,9 @@ import Crawl from '../components/crawl/crawl.jsx';
 import Signup from '../components/authentication/signup.jsx';
 import Login from '../components/authentication/login.jsx';
 
-const Root = ({ store, history }) => (
+const Root = ({ store }) => (
   <Provider store={store}>
-    <Router history={ history }>
+    <Router history={ createBrowserHistory() }>
       <Route component={ App }>
         <IndexRoute component={ StopList } />
         <Route path="find" component={ CrawlList } />
