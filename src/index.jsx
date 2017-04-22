b9c4074ef997137e8e487182b25ec236f2cf2768
Hans-Olav Hessen <hansolav_91@hotmail.com>
2017-04-22 13:02:16 +0200

    Removed logger and added inferno devtools

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import Inferno from 'inferno';
 import createHistory from 'history/createHashHistory';
+import 'inferno-devtools';
 import Root from './app/root.jsx';
 import configureStore from './app/configureStore';
 import './app/styles';
@@ -7,6 +8,10 @@ import './app/styles';
 const rootEl = document.getElementById('root');
 const store = configureStore();
 const history = createHistory();
+
+if (module.hot) {
+  require('inferno-devtools'); // eslint-disable-line global-require
+}
 
 Inferno.render(
   <Root store={store} history={history} />
